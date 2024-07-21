28f4c79414bc99d9ebf8f7fd6bd010d612466b23
César Infante <cesarinfantedocampo@gmail.com>
2024-07-21 17:54:18 +0200

    feat: añade ejemplos de botones
    #ui-button

--- angular-forms/src/app/app.component.ts
+++ angular-forms/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReusableFormGroupsOriginalComponent } from '../components/reusable-form-groups/reusable-form-groups-original/reusable-form-groups-original.component';
 import { ReusableFormGroupsComponent } from '../components/reusable-form-groups/reusable-form-groups/reusable-form-groups.component';
+import { UiButtonComponent } from '../components/ui/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReusableFormGroupsOriginalComponent, ReusableFormGroupsComponent],
+  imports: [RouterOutlet, ReusableFormGroupsOriginalComponent, ReusableFormGroupsComponent, UiButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
